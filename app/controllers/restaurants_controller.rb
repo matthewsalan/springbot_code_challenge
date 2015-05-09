2949b3950412753485ac4859fe3b0bcea50a2377
Alan Matthews <matthewsa1320@gmail.com>
2015-05-09 02:12:16 -0400

    adding restaurants controller; restaurant model validations; necessary routes; adding views for restaurants controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,26 @@
 class RestaurantsController < ApplicationController
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    respond_to do |format|
+      if @restaurant.save
+        format.html { redirect_to restaurants_path, notice: 'Restaurant was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  private
+    def restaurant_params
+      params.require(:restaurant).permit(:name, :rating, :last_visited)
+    end
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,7 @@
 class Restaurant < ActiveRecord::Base
   has_many :users
+  validates :name, presence: true
+  validates :rating, numericality: { only_integer: true }
+  validates :rating, presence: { message: "Rating must bbe between 1 - 5" }
+  validates_numericality_of :rating, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 5
 end
