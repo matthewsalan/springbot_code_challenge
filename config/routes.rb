ec1c87a86185d69558aca16fb1467c7b5c26f329
Alan Matthews <matthewsa1320@gmail.com>
2015-05-15 14:37:49 -0400

    changing root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'ratings#index'
+  root to: 'restaurants#index'
   devise_for :users
   resources :restaurants, only: [:index, :new, :create, :show] do
     resources :ratings, only: [:create, :new]
