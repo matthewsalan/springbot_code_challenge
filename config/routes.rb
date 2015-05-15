befd161a28a33ab97a1032bfd577dced6da5f99a
Alan Matthews <matthewsa1320@gmail.com>
2015-05-15 15:25:42 -0400

    changing root to

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'restaurants#index'
+  root to: 'ratings#index'
   devise_for :users
   resources :restaurants, only: [:index, :new, :create, :show] do
     resources :ratings, only: [:create, :new]
