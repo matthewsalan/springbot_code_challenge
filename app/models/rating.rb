2ba4c3f294c004ba3cc150a023e9f912d640dc2d
Alan Matthews <matthewsa1320@gmail.com>
2015-05-15 15:18:05 -0400

    styling new ratings view; changing validations on ratings model

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -4,7 +4,6 @@ class Rating < ActiveRecord::Base
   validates :restaurant_id, uniqueness: { scope: :user_id,
     message: "has been rated by you.  One rating per user per restaurant " }
   validates :rating, numericality: { only_integer: true }
-  validates :rating, presence: true
-  validates :rating, presence: { message: "Rating must be between 1 - 5" }
-  validates_numericality_of :rating, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 5
+  validates :rating, presence: true 
+  validates_numericality_of :rating, { :greater_than_or_equal_to => 1, :less_than_or_equal_to => 5, message: "Rating must be between 1 - 5" }
 end
