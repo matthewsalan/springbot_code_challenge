c6a028fc005506356010787f8ca78e752c1125d8
Alan Matthews <matthewsa1320@gmail.com>
2015-05-19 02:02:51 -0400

    adding tests for restaurants controller

--- test/controllers/restaurants_controller_test.rb
+++ test/controllers/restaurants_controller_test.rb
@@ -1,4 +1,15 @@
 require 'test_helper'
 
 class RestaurantsControllerTest < ActionController::TestCase
+  include Devise::TestHelpers
+
+  def test_index
+    get :index
+    assert_response :success
+  end
+
+  def test_show
+    get :show, id: restaurants(:two)
+    assert_response :success
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,4 +1,18 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+  describe User do
+    let(:user_params) { {email: "a@a.com", password: "password", password_confirmation: "password"} } 
+    let(:user) {User.new user_params}
+
+    it "is valid new user with valid params" do 
+      user.must_be :valid?
+    end
+
+    it "is invalid without an email" do 
+      user_params.delete :email
+      user.wont_be :valid?
+      user.errors[:email].must_be :present?
+    end
+  end
 end
